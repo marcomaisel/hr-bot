1a66e95fb55349ba2fe9f3731d62acc109eb1710
Marco Maisel <maiselmarco@gmail.com>
2018-09-21 20:27:13 +0200

    Add more technologies

--- custom_actions/actions.py
+++ custom_actions/actions.py
@@ -186,28 +186,4 @@ class ActionMatchJobSlots(Action):
                     SlotSet("possibleTasks", possibleTasks),
                     SlotSet("possibleDomains", possibleDomains),
                     FollowupAction("utter_askTask")]
-
-            # for job in jobs:
-            #     # Store the values of each key for all job dictionaries in the jobs list
-            #     tech = job["technology"]
-            #     task = job["jobTask"]
-            #     metatech = job["domain"]
-            #     employment = job["formOfEmployment"]
-
-            #     # Search for matches between slots sent by the user and stored jobs in the jobs list.
-            #     # If a job has at least one value for each key in the dictionary the job is appended to the foundJobs list.
-            #     if [i for i in technologySlot if i in tech] and \
-            #        [i for i in domainSlot if i in metatech] and \
-            #        taskSlot in task and \
-            #        formOfEmploymentSlot in employment:
-            #         foundJobs.append(job)
-
-            # # Create a message for the user with a list of names of all found jobs, separated by comma
-            # jobMessage = ", ".join([c["name"] for c in foundJobs])
-
-            # # Send message with all found jobs to the user with the dispatcher object
-            # dispatcher.utter_message("{}".format(jobMessage))
-
-            # Set jobs slot with all found jobs
-            # return [SlotSet("jobs", foundJobs if foundJobs is not None else [])]
         return
