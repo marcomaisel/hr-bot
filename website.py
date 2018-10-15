98242b3de3a4560151c646fb5eeb6e16a42b2863
Marco Maisel <maiselmarco@gmail.com>
2018-10-15 16:59:52 +0200

    Add working mongodb as trackerstore for logging purposes

--- website.py
+++ website.py
@@ -2,19 +2,24 @@ from rasa_core.agent import Agent
 from rasa_core.channels.socketio import SocketIOInput
 from rasa_core.tracker_store import MongoTrackerStore
 from rasa_core.domain import TemplateDomain
+from rasa_core.interpreter import (
+    NaturalLanguageInterpreter)
 import os
 import sys
 
-domain = TemplateDomain.load(os.path.join('./models/dialogue', "domain.yml"))
+domain = TemplateDomain.load(os.path.join("models/dialogue", "domain.yml"))
 
+# _interpreter = NaturalLanguageInterpreter.create("models/current/nlu/")
 
-# tracker_store = MongoTrackerStore(domain, host="mongodb://localhost:27017",
-#                                   db="rasa",
-#                                   username=None,
-#                                   password=None,
-#                                   collection="conversations")
+_tracker_store = MongoTrackerStore(domain,
+                                   host="mongodb://localhost:27017",
+                                   db="rasa",
+                                   username=None,
+                                   password=None,
+                                   collection="conversations")
 
-# agent = Agent.load("models/dialogue/", "models/current/nlu/", tracker_store)
-agent = Agent.load("models/dialogue/", "models/current/nlu/")
+agent = Agent.load("models/dialogue/", "models/current/nlu/",
+                   #    interpreter=_interpreter,
+                   tracker_store=_tracker_store)
 
 agent.handle_channels([SocketIOInput()], 5500, serve_forever=True)
